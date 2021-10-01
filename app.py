affaf6d7c1ffd4bc50e70a4d1a537b6902013a53
aswin lal <aswnss.m@gmail.com>
2021-10-01 07:20:24 +0530

    unfinished bussiness

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import os
 
 app = Flask(__name__)
 
+base_dir = os.getcwd() + '/projects' #projects folder
+sub_folders = os.listdir(base_dir) #list of subfolders
+index_dirs = [sub_folder + '/index.html' for sub_folder in sub_folders] #index.html path    
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/projects/')
 def projects():
-    base_dir = os.getcwd() + '/projects' #projects folder
-    sub_folders = os.listdir(base_dir) #list of subfolders
-    index_dirs = [sub_folder + '/index.html' for sub_folder in sub_folders]
+
     return render_template('new_project.html', files= index_dirs)
 
 @app.route('/projects/<string:folder_name>/')
